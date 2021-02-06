4bd8dbed8c13156931303508e0bd4728576acda0
antdp425 <antdp425@aol.com>
2021-02-06 07:07:52 -0500

    adds functionality to update player names + basic styling

--- src/components/Lineup.js
+++ src/components/Lineup.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Player from './Player'
+import React from "react";
+import Player from "./Player";
 
 function Lineup() {
-   return (
-      <div className="lineup">
-         <Player goalie={true}defaultPosition={{x:400,y:50}}/>
-         <Player defaultPosition={{x:175,y:150}}/>
-         <Player defaultPosition={{x:325,y:150}}/>
-         <Player defaultPosition={{x:475,y:150}}/>
-         <Player defaultPosition={{x:625,y:150}}/>
-         <Player defaultPosition={{x:250,y:250}}/>
-         <Player defaultPosition={{x:400,y:250}}/>
-         <Player defaultPosition={{x:550,y:250}}/>
-         <Player defaultPosition={{x:250,y:350}}/>
-         <Player defaultPosition={{x:400,y:350}}/>
-         <Player defaultPosition={{x:550,y:350}}/>
-      </div>
-   )
+  return (
+    <div className="lineup">
+      <Player goalie={true} defaultPosition={{ x: 400, y: 50 }} />
+      <Player defaultPosition={{ x: 175, y: 150 }} />
+      <Player defaultPosition={{ x: 325, y: 150 }} />
+      <Player defaultPosition={{ x: 475, y: 150 }} />
+      <Player defaultPosition={{ x: 625, y: 150 }} />
+      <Player defaultPosition={{ x: 250, y: 250 }} />
+      <Player defaultPosition={{ x: 400, y: 250 }} />
+      <Player defaultPosition={{ x: 550, y: 250 }} />
+      <Player defaultPosition={{ x: 250, y: 350 }} />
+      <Player defaultPosition={{ x: 400, y: 350 }} />
+      <Player defaultPosition={{ x: 550, y: 350 }} />
+    </div>
+  );
 }
-export default Lineup
+export default Lineup;
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,21 +2,40 @@ import React, { useState, useEffect, useRef } from "react";
 import Draggable from "react-draggable";
 
 function Player({ goalie, defaultPosition }) {
+  let playerNameField = useRef();
+  let focusOnTextField = () => playerNameField.current.select();
+
+  let [playerName, setPlayerName] = useState();
+  let handleChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
   return (
     <Draggable
       defaultPosition={{ x: defaultPosition.x, y: defaultPosition.y }}
       bounds="parent"
     >
-    {!goalie ?
-      <div className="player">
-        <i class="fas fa-tshirt fa-2x"></i>
-        <div>playerName</div>
-      </div> : 
-      <div className="player">
-        <i class="goalie fas fa-tshirt fa-2x"></i>
-        <div>playerName</div>
-      </div>
-    }
+      {!goalie ? (
+        <div onClick={focusOnTextField} className="player">
+          <i class="outfield fas fa-tshirt fa-2x"></i>
+          <input
+            type="text"
+            onChange={handleChange}
+            ref={playerNameField}
+            value={playerName}
+          />
+        </div>
+      ) : (
+        <div onClick={focusOnTextField} className="player">
+          <i class="goalie fas fa-tshirt fa-2x"></i>
+          <input
+            type="text"
+            onChange={handleChange}
+            ref={playerNameField}
+            value={playerName}
+          />
+        </div>
+      )}
     </Draggable>
   );
 }
